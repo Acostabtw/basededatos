ef560663ad76bc1e4f92cc4fb3194b2789def88a
Acostabtw <sebastianacosta.grimont@gmail.com>
2026-03-17 22:18:04 -0400

    Agregar archivos de proyecto.

---
+++ basededatos/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace basededatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_agregar = new System.Windows.Forms.Button();
+            this.dataGridView_fila = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Num_Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre_txt = new System.Windows.Forms.TextBox();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label_apellido = new System.Windows.Forms.Label();
+            this.apellido_txt = new System.Windows.Forms.TextBox();
+            this.label_num = new System.Windows.Forms.Label();
+            this.telefono_txt = new System.Windows.Forms.TextBox();
+            this.Btn_eliminar = new System.Windows.Forms.Button();
+            this.Btn_ver = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripDropDownButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_fila)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn_agregar
+            // 
+            this.Btn_agregar.Location = new System.Drawing.Point(1098, 35);
+            this.Btn_agregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_agregar.Name = "Btn_agregar";
+            this.Btn_agregar.Size = new System.Drawing.Size(78, 54);
+            this.Btn_agregar.TabIndex = 3;
+            this.Btn_agregar.Text = "agregar";
+            this.Btn_agregar.UseVisualStyleBackColor = true;
+            this.Btn_agregar.Click += new System.EventHandler(this.Btn_agregar_Click);
+            // 
+            // dataGridView_fila
+            // 
+            this.dataGridView_fila.AllowUserToAddRows = false;
+            this.dataGridView_fila.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_fila.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Apellido,
+            this.Num_Telefono});
+            this.dataGridView_fila.Location = new System.Drawing.Point(373, 209);
+            this.dataGridView_fila.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView_fila.Name = "dataGridView_fila";
+            this.dataGridView_fila.RowHeadersWidth = 51;
+            this.dataGridView_fila.RowTemplate.Height = 24;
+            this.dataGridView_fila.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_fila.Size = new System.Drawing.Size(606, 343);
+            this.dataGridView_fila.TabIndex = 4;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 125;
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.MinimumWidth = 6;
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Width = 125;
+            // 
+            // Num_Telefono
+            // 
+            this.Num_Telefono.HeaderText = "Numero de telefono";
+            this.Num_Telefono.MinimumWidth = 6;
+            this.Num_Telefono.Name = "Num_Telefono";
+            this.Num_Telefono.Width = 125;
+            // 
+            // nombre_txt
+            // 
+            this.nombre_txt.Location = new System.Drawing.Point(454, 35);
+            this.nombre_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nombre_txt.Name = "nombre_txt";
+            this.nombre_txt.Size = new System.Drawing.Size(104, 20);
+            this.nombre_txt.TabIndex = 0;
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Location = new System.Drawing.Point(336, 38);
+            this.label_name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(47, 13);
+            this.label_name.TabIndex = 3;
+            this.label_name.Text = "Nombre:";
+            // 
+            // label_apellido
+            // 
+            this.label_apellido.AutoSize = true;
+            this.label_apellido.Location = new System.Drawing.Point(336, 76);
+            this.label_apellido.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_apellido.Name = "label_apellido";
+            this.label_apellido.Size = new System.Drawing.Size(47, 13);
+            this.label_apellido.TabIndex = 4;
+            this.label_apellido.Text = "Apellido:";
+            // 
+            // apellido_txt
+            // 
+            this.apellido_txt.Location = new System.Drawing.Point(454, 73);
+            this.apellido_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.apellido_txt.Name = "apellido_txt";
+            this.apellido_txt.Size = new System.Drawing.Size(104, 20);
+            this.apellido_txt.TabIndex = 1;
+            // 
+            // label_num
+            // 
+            this.label_num.AutoSize = true;
+            this.label_num.Location = new System.Drawing.Point(331, 114);
+            this.label_num.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_num.Name = "label_num";
+            this.label_num.Size = new System.Drawing.Size(52, 13);
+            this.label_num.TabIndex = 6;
+            this.label_num.Text = "Telefono:";
+            // 
+            // telefono_txt
+            // 
+            this.telefono_txt.Location = new System.Drawing.Point(454, 111);
+            this.telefono_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.telefono_txt.Name = "telefono_txt";
+            this.telefono_txt.Size = new System.Drawing.Size(104, 20);
+            this.telefono_txt.TabIndex = 2;
+            // 
+            // Btn_eliminar
+            // 
+            this.Btn_eliminar.Location = new System.Drawing.Point(1098, 114);
+            this.Btn_eliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_eliminar.Name = "Btn_eliminar";
+            this.Btn_eliminar.Size = new System.Drawing.Size(78, 50);
+            this.Btn_eliminar.TabIndex = 6;
+            this.Btn_eliminar.Text = "eliminar";
+            this.Btn_eliminar.UseVisualStyleBackColor = true;
+            this.Btn_eliminar.Click += new System.EventHandler(this.Btn_eliminar_Click);
+            // 
+            // Btn_ver
+            // 
+            this.Btn_ver.Location = new System.Drawing.Point(1098, 209);
+            this.Btn_ver.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_ver.Name = "Btn_ver";
+            this.Btn_ver.Size = new System.Drawing.Size(78, 50);
+            this.Btn_ver.TabIndex = 7;
+            this.Btn_ver.Text = "Ver registros";
+            this.Btn_ver.UseVisualStyleBackColor = true;
+            this.Btn_ver.Click += new System.EventHandler(this.Btn_ver_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 585);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1302, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(131, 20);
+            this.lblStatus.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1302, 607);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.Btn_ver);
+            this.Controls.Add(this.Btn_eliminar);
+            this.Controls.Add(this.telefono_txt);
+            this.Controls.Add(this.label_num);
+            this.Controls.Add(this.apellido_txt);
+            this.Controls.Add(this.label_apellido);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.nombre_txt);
+            this.Controls.Add(this.dataGridView_fila);
+            this.Controls.Add(this.Btn_agregar);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_fila)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_agregar;
+        private System.Windows.Forms.DataGridView dataGridView_fila;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num_Telefono;
+        private System.Windows.Forms.TextBox nombre_txt;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label_apellido;
+        private System.Windows.Forms.TextBox apellido_txt;
+        private System.Windows.Forms.Label label_num;
+        private System.Windows.Forms.TextBox telefono_txt;
+        private System.Windows.Forms.Button Btn_eliminar;
+        private System.Windows.Forms.Button Btn_ver;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton lblStatus;
+    }
+}
+
---
+++ basededatos/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace basededatos
+{
+    public partial class Form1 : Form
+    {
+        string cadenaConexion = "Server=127.0.0.1; Port=3306; Database=Curso; User ID=root; Password=;";
+        public Form1()
+        {
+            InitializeComponent();
+            VerificarConexion();
+        }
+
+        private void Btn_agregar_Click(object sender, EventArgs e)
+        {
+            // 3. Insertar datos en MySQL
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    string query = "INSERT INTO registro (nombre, apellido, numero_telefono) VALUES (@nom, @ape, @tel)";
+                    MySqlCommand cmd = new MySqlCommand(query, conexion);
+
+                    // Usamos parámetros para evitar inyección SQL
+                    cmd.Parameters.AddWithValue("@nom", nombre_txt.Text);
+                    cmd.Parameters.AddWithValue("@ape", apellido_txt.Text);
+                    cmd.Parameters.AddWithValue("@tel", telefono_txt.Text);
+
+                    cmd.ExecuteNonQuery();
+                    CargarDatos();
+
+                    // 4. Si se guarda en la BD, lo agregamos a la tabla visual (DataGrid)
+                   // dataGridView_fila.Rows.Add(nombre_txt.Text, apellido_txt.Text, telefono_txt.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar con la base de datos: " + ex.Message);
+                }
+            }
+
+            // Limpieza de campos (tu código original)
+            nombre_txt.Clear();
+            apellido_txt.Clear();
+            telefono_txt.Clear();
+            nombre_txt.Focus();
+
+            SystemSounds.Exclamation.Play();
+        }
+
+        /* private void Btn_agregar_Click(object sender, EventArgs e)
+         {
+             dataGridView_fila.Rows.Add(nombre_txt.Text, apellido_txt.Text, telefono_txt.Text);
+             nombre_txt.Clear();
+             apellido_txt.Clear();
+             telefono_txt.Clear();
+             nombre_txt.Focus();
+
+             SystemSounds.Exclamation.Play();
+         }*/
+
+
+        private void Btn_eliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView_fila.CurrentRow == null)
+            {
+                MessageBox.Show("Selecciona una fila para eliminar.");
+                return;
+            }
+
+            // Obtenemos el nombre de la fila seleccionada
+            string nombreAEliminar = dataGridView_fila.CurrentRow.Cells[0].Value.ToString();
+
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    // Borramos de la tabla 'usuarios' donde el nombre coincida
+                    string query = "DELETE FROM registro WHERE nombre = @nom";
+                    MySqlCommand cmd = new MySqlCommand(query, conexion);
+                    cmd.Parameters.AddWithValue("@nom", nombreAEliminar);
+
+                    cmd.ExecuteNonQuery();
+
+                    // Si se borró en MySQL, lo quitamos de la vista
+                    dataGridView_fila.Rows.Remove(dataGridView_fila.CurrentRow);
+                    MessageBox.Show("Registro eliminado de la base de datos.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al eliminar: " + ex.Message);
+                }
+            }
+        }
+
+        private void Btn_ver_Click(object sender, EventArgs e)
+        {
+            
+            if (dataGridView_fila.Rows.Count > 0)
+            {
+                
+                dataGridView_fila.Rows.Clear();
+                Btn_ver.Text = "Ver registros"; 
+                MessageBox.Show("Registros ocultados");
+            }
+            else
+            {
+                
+                string passwordCorrecta = "1234";
+                string claveIngresada = Microsoft.VisualBasic.Interaction.InputBox("Introduce la contraseña de administrador:", "Acceso Restringido", "");
+
+                if (claveIngresada == passwordCorrecta)
+                {
+                    MessageBox.Show("Acceso concedido");
+                    CargarDatos();
+                    Btn_ver.Text = "Ocultar registros"; 
+                }
+                else
+                {
+                    MessageBox.Show("Contraseña incorrecta. Acceso denegado.");
+                }
+            }
+        }
+
+        private void CargarDatos()
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    string query = "SELECT nombre, apellido, numero_telefono FROM registro";
+                    MySqlCommand cmd = new MySqlCommand(query, conexion);
+                    MySqlDataAdapter adaptador = new MySqlDataAdapter(cmd);
+
+                    DataTable dt = new DataTable();
+                    adaptador.Fill(dt);
+
+                    dataGridView_fila.Rows.Clear();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        dataGridView_fila.Rows.Add(row["nombre"], row["apellido"], row["numero_telefono"]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al cargar datos: " + ex.Message);
+                }
+            }
+        }
+
+        private void VerificarConexion()
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    // Si logra abrirse:
+                    lblStatus.Text = "Base de Datos: Conectada";
+                    lblStatus.ForeColor = Color.DarkGreen;
+                }
+                catch (Exception)
+                {
+                    // Si falla:
+                    lblStatus.Text = "Base de Datos: Desconectada";
+                    lblStatus.ForeColor = Color.Red;
+                }
+            }
+        }
+
+    } // Esta es la llave que cierra la clase Form1 (NO LA BORRES)
+}
+
+        /* private void Btn_eliminar_Click(object sender, EventArgs e)
+         {
+             if (dataGridView_fila == null)
+             {
+                 MessageBox.Show("Escoge una");
+                 return;
+
+             }
+
+             dataGridView_fila.Rows.Remove(dataGridView_fila.CurrentRow);
+         }*/
+
+
